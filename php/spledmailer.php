2c21b35c9f75945faf352830591187ca19a35075
Mrapagao <76593197+Mrapagao@users.noreply.github.com>
2021-11-17 22:56:41 +0000

    8th

--- php/spledmailer.php
+++ php/spledmailer.php
@@ -1,30 +1,26 @@
-<?php
-@extract($_POST);
-$admin = 'comercial@marfoto.net' ; // Change to your admin email 'from' address
-$subject = 'Formulario do Website'; //Your email subject
-$name = spledmailer($fname); //can be stripslashes('name');
-$email = spledmailer($email);
-// Your HTML message with table, links and images
-$message = '
-<html>
-<head>
-<title>HTML & CSS Tutorial</title>
-</head>
-<body>
-<p style="color: red;"><img src="http://www.wickham43.net/images/orangew90x90.jpg" alt="Orange W"> Here are some urls regarding my tutorial</p>
-<table border="1" cellspacing="2" cellpadding="20" bgcolor="#eeeeee">
-<tr>
-<th>Introduction</th><th>Contents</th><th>Useful Sites</th>
-</tr>
-<tr>
-<td><img src="http://www.wickham43.net/images/green16x16.jpg" alt="Green Square"/> <a href="http://www.wickham43.net/">Introduction</a></td>
-<td><img src="http://www.wickham43.net/images/green16x16.jpg" alt="Green Square"/> <a href="http://www.wickham43.net/contents.php">Contents</a></td>
-<td><img src="http://www.wickham43.net/images/green16x16.jpg" alt="Green Square"/> <a href="http://www.wickham43.net/lists.php">Lists</a></td>
-</tr>
-</table>
-</body>
-</html>
-';
+<?php 
+if(isset($_POST['submit'])){
+    $to = "mrapagao@egmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['fname'];
+    $last_name = $_POST['lname'];
+    $phone = $_post ['phone'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " " . $phone ."\n\n" . $_POST['message'] . "\n\n" . $_POST['message2'];
+    $message2 = "Obrigado pelo seu contacto " ."\n\n" . $first_name . " " . $last_name . " " . $phone . "\n\n" . $_POST['message'] . "\n\n" . $_POST['message2'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Email enviado. Obrigado" . $first_name . "\n\n" ."Iremos contacta-lo em breve.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
+
+
+<!--
 // To send HTML mail, the Content-type header must be set
 $headers = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
@@ -40,3 +36,5 @@ else {
 echo "ERROR";
 }
 ?>
+
+-->
